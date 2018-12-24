254156a076e6a5b41d816215166e9c5e5f061844
Sheesho <37927049+Sheesho@users.noreply.github.com>
2018-12-24 17:13:22 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -20,7 +20,7 @@ bot.on('ready', () => {
 	bot.user.setStatus('online', '!guide for help')
 	console.log('Connected');
 	console.log('Carmilla is ready to go');
-	bot.user.setActivity("!guide for help")
+	bot.user.setActivity("!guide for help");
 	bot.channels.get("431470143902580736").send("<@250342596784816129>, I am online again ! <:blobkiss:375218160225222656>");
 });
 
