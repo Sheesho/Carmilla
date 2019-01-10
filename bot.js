dafd6fc8b7a32c5f9609444c7f49e7a1dbff0b29
Sheesho <37927049+Sheesho@users.noreply.github.com>
2019-01-10 21:47:02 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -90,70 +90,174 @@ bot.on('ready', () => {
 	}
 });*/
 
-bot.on('message', (message) => 
-{
-	var emote = '<:blobkiss:375218160225222656>';	
-	if(message.guild.id == auth.umi)
+bot.on('messageUpdate', (oldMessage, newMessage) => {
+	console.log(newMessage.content);
+   if((newMessage.content.startsWith(auth.prefix + 'emote')) && (!newMessage.author.bot))
 	{
-		emote = bot.emojis.find("name", "yug").toString();
-	}
-	var special = "";
-	
-	
-
-	if ((message.content.substring(0,1) == auth.prefix) && (message.author.id != bot.id))
-	{
-		var cmd = "";
-        	cmd = message.content.substring(1,message.length);
-        	var special = "";
-		//xmas			
-		if(message.content.startsWith(auth.prefix + 'xmas') && (!message.author.bot))
+       {
+		var bool = false;
+		var cont = newMessage.content.substring(7,newMessage.length);
+		urlExists(auth.emotes+cont+".png", function(err, exists)
 		{
-			message.delete();
-			urlExists("https://raw.githubusercontent.com/Sheesho/Carmilla/master/Xmas-Carmilla.png", function(err)
+			if(exists)
 			{
+				bool = true;
 				//Promise
 				var promise = new Promise(function(resolve, reject) 
 				{
 					// do a thing, possibly async, then…
-					resolve(bot.channels.get("293107888040181780").send("Merry Christmas Everyone !\nCarmilla is super-duper happy about the time she spent here <:vampou:375604910655275026>.\nI wish you all to have an amazing time eating chocolate and opening presents <:blobnom:375604908940066818> !\nCan't wait to have more fun for New year and all the years that will follow, yay ! I cannot give you a present so I hope a kiss from this cutie will be enought <:blobkiss:375604900119314432>\n<:vampou:375604910655275026> Carmilla's out ! <:vampou:375604910655275026>\n\nP.S:Shisho said that if you guys react with :gift: I will get lots and lots of nice things so pwease give a lot to the little Carmi-nya ! :sparkles:", {file: "https://raw.githubusercontent.com/Sheesho/Carmilla/master/Xmas-Carmilla.png"}).catch(
+					resolve(newMessage.channel.send("", {file: auth.emotes + cont + ".png"}).catch(
 					function() 
 					{ 
 						console.log("promesse rompue");
 					}));
-				//End of promise
 				});
-			});
-		   }
-		   //the guide command
-		if(message.content.startsWith(auth.prefix + 'guide'))
-		{
-			if(message.guild.id == "492355440399745026")
-			{	
-				const helpembed = new Discord.RichEmbed()
-					.setTitle("Carmilla (la meilleure des bots discord <:blobkiss:375218160225222656>) super guide")
-					.setDescription("Tiens tiens tiens, on dirait que quelqu'un a besoin d'informations sur ce que peut faire l'incroyable & adorable Carmilla~ <:blobpeek:375218261660401664>\nVoici la liste de ce que je peux faire(Et dites à Shisho de se dépêcher d'en ajouter d'autres):")
-					.setColor("#1ce6af")
-					.setFooter("Carmilla bot est propriété de Shisho#7817, Soyez gentils s'il vous plait ⎛　　　　´●　　ω　●`　 ⎞")
-					.addField("!say", "Si vous aimez faire dire des choses stupides aux gens cette commande est faîte pour vous~ <:blobkiss:375218160225222656>")
-					.addField("!emote", "Si vous voulez utiliser tout plein d'emojis super sympas, parce qu'être limité à 50 c'est pas fun <:blobtear:375218561628766210>. Vous n'avez qu'à en choisir une dans l'incroyable [liste des emojis](https://docs.google.com/spreadsheets/d/1VtGPAa2QJU5IUZdEdJpj4eQVoz4aMSS9yNqCCTsWpEI/edit?usp=sharing), tout comme la commande précédente, elle est sensible à la casse~ <:blobkiss:375218160225222656>\n\n\n\nJ'espère que ça vous a été utile et que l'on va beacoup s'amuser ensemble.<:blobpeek:375218261660401664>\n\nOh, J'ai faillis oublier de vous souhaiter une bonne journée, quelle étourdie je fais~");
-				message.channel.send({ embed: helpembed });
+				//End of promise
 			}
-			else
+		});
+		urlExists(auth.emotes+cont+".gif", function(err, exists)
+		{
+			if(exists)
 			{
-				const helpembed = new Discord.RichEmbed()
-					.setTitle("Carmilla (cutest discord bot <:blobkiss:375218160225222656>) guide")
-					.setDescription("Well, looks like someone needs some pointers on how to interact with the cute little m... I-I mean on how to interact with the wonderful lady that Carmilla is~ <:blobpeek:375218261660401664>\nHere is a short description of all the sentences a proper person should use to address a lady:")
-					.setColor("#1ce6af")
-					.setFooter("Carmilla bot is property of Shisho#7817, no bully please ⎛　　　　´●　　ω　●`　 ⎞")
-					.addField("!hello", "If you're ever feeling lonely because nobody is saying hi to you you can count on me ! <:blobkiss:375218160225222656>")
-					.addField("!say", "If you want to make me say silly things, you can use this~ <:blobkiss:375218160225222656>")
-					.addField("!role", "If you want to add a cool role to the list of those you already have or remove one because it's not good enough for you just use this followed by that specific role. Beware, it is case sen-si-ti-ve~ <:blobkiss:375218160225222656>")
-					.addField("!emote", "If you want to use nice emotes because being restricted to just 50 is no fun <:blobtear:375218561628766210>. Just pick an emote from the extensive [emote list](https://docs.google.com/spreadsheets/d/1VtGPAa2QJU5IUZdEdJpj4eQVoz4aMSS9yNqCCTsWpEI/edit?usp=sharing), similarily to the previous command this one is case sen-si-ti-ve~ <:blobkiss:375218160225222656>\n\n\n\nI hope this has been helpful to you and that you're gonna have a lot of fun playing with me from now on<:blobpeek:375218261660401664>\n\nOh, I almost forgot that I shouldn't speak about the secret commands & interactions, what a klutz I am teehee~");
-				message.channel.send({ embed: helpembed });
-			}	
+				bool = true;
+				//Promise
+				var promise = new Promise(function(resolve, reject) 
+				{
+					// do a thing, possibly async, then…
+					resolve(newMessage.channel.send("", {file: auth.emotes + cont + ".gif"}).catch(
+					function() 
+					{ 
+						console.log("promesse rompue");
+					}));
+				});
+				//End of promise
+			}
+		});
+	}
+   }
+});
+
+bot.on('message', (message) => {
+   var emote = '<:blobkiss:375218160225222656>';	
+   if(message.guild.id == auth.umi)
+   {
+	emote = bot.emojis.find("name", "yug").toString();
+   }
+	var special = "";
+	if((message.author.id == auth.owner) && (message.content.startsWith(auth.prefix+"h")))
+	{
+		message.delete();
+		var value = parseInt(message.content.substring(3,message.length), 10);
+		if(value == NaN)
+		{
+			value = 50;
 		}
-		//getting the hello command to work with stuff at the end.
+		hate = value;
+		console.log('hate set to ' + hate); 
+	}
+	if(message.author.id == "237111771368914945")
+    {
+		if(hate > 0)
+		{
+			hate = hate-1;
+			console.log('hate remaining: ' + hate); 
+			message.delete();
+		}
+			
+	}
+	else
+	{
+		if ((message.content.substring(0,1) == auth.prefix) && (message.author.id != bot.id))
+	{
+		var cmd = "";
+        cmd = message.content.substring(1,message.length);
+		console.log("cmd: " + cmd);
+        var special = "";
+        switch(cmd) {
+				
+			//the howyoudoing command.
+			
+			case 'how are you doing':
+				special = 'Carmilla is always happy ' + emote;
+				switch(message.author.id){
+				case auth.owner:
+					special = "Carmilla is always happy, thanks for the concern Mr Developer " + emote;
+				break;
+				case auth.giggles:
+					special = "Carmilla is always happy Onii-chan " + emote;
+				break;
+				case auth.yoruno:
+					special = "*blushes* I am very happy right now " + emote;
+				break;
+				case auth.neppy: 
+					special = "Carmilla is always happy Uncle Nâz " + emote;
+				break;
+				case auth.udon: 
+					special = "Carmilla is always happy Danchou " + emote;
+				break;
+				case auth.yuki: 
+					special = "Carmilla is ready to invade Poland mein Fuhrer " + emote;
+				break;
+				}
+				message.channel.send(special);
+			break;
+			case 'how':
+				special = 'Carmilla is always happy ' + emote;
+				switch(message.author.id){
+				case auth.owner:
+					special = "Carmilla is always happy, thanks for the concern Mr Developer " + emote;
+				break;
+				case auth.giggles:
+					special = "Carmilla is always happy Onii-chan " + emote;
+				break;
+				case auth.yoruno:
+					special = "*blushes* I am very happy right now " + emote;
+				break;
+				case auth.neppy: 
+					special = "Carmilla is always happy Uncle Nâz " + emote;
+				break;
+				case auth.udon: 
+					special = "Carmilla is always happy Danchou " + emote;
+				break;
+				case auth.yuki: 
+					special = "Carmilla is ready to invade Poland mein Fuhrer " + emote;
+				break;
+				}
+				message.channel.send(special);
+			break;	
+            // Just add any case commands if you want to..
+        }
+	}
+	//the guide command
+	if(message.content.startsWith(auth.prefix + 'guide'))
+	{
+		if(message.guild.id == "492355440399745026")
+		{	
+			const helpembed = new Discord.RichEmbed()
+				.setTitle("Carmilla (la meilleure des bots discord <:blobkiss:375218160225222656>) super guide")
+				.setDescription("Tiens tiens tiens, on dirait que quelqu'un a besoin d'informations sur ce que peut faire l'incroyable & adorable Carmilla~ <:blobpeek:375218261660401664>\nVoici la liste de ce que je peux faire(Et dites à Shisho de se dépêcher d'en ajouter d'autres):")
+				.setColor("#1ce6af")
+				.setFooter("Carmilla bot est propriété de Shisho#7817, Soyez gentils s'il vous plait ⎛　　　　´●　　ω　●`　 ⎞")
+				.addField("!say", "Si vous aimez faire dire des choses stupides aux gens cette commande est faîte pour vous~ <:blobkiss:375218160225222656>")
+				.addField("!emote", "Si vous voulez utiliser tout plein d'emojis super sympas, parce qu'être limité à 50 c'est pas fun <:blobtear:375218561628766210>. Vous n'avez qu'à en choisir une dans l'incroyable [liste des emojis](https://docs.google.com/spreadsheets/d/1VtGPAa2QJU5IUZdEdJpj4eQVoz4aMSS9yNqCCTsWpEI/edit?usp=sharing), tout comme la commande précédente, elle est sensible à la casse~ <:blobkiss:375218160225222656>\n\n\n\nJ'espère que ça vous a été utile et que l'on va beacoup s'amuser ensemble.<:blobpeek:375218261660401664>\n\nOh, J'ai faillis oublier de vous souhaiter une bonne journée, quelle étourdie je fais~");
+			message.channel.send({ embed: helpembed });
+		}
+		else
+		{
+				const helpembed = new Discord.RichEmbed()
+				.setTitle("Carmilla (cutest discord bot <:blobkiss:375218160225222656>) guide")
+				.setDescription("Well, looks like someone needs some pointers on how to interact with the cute little m... I-I mean on how to interact with the wonderful lady that Carmilla is~ <:blobpeek:375218261660401664>\nHere is a short description of all the sentences a proper person should use to address a lady:")
+				.setColor("#1ce6af")
+				.setFooter("Carmilla bot is property of Shisho#7817, no bully please ⎛　　　　´●　　ω　●`　 ⎞")
+				.addField("!hello", "If you're ever feeling lonely because nobody is saying hi to you you can count on me ! <:blobkiss:375218160225222656>")
+				.addField("!say", "If you want to make me say silly things, you can use this~ <:blobkiss:375218160225222656>")
+				.addField("!role", "If you want to add a cool role to the list of those you already have or remove one because it's not good enough for you just use this followed by that specific role. Beware, it is case sen-si-ti-ve~ <:blobkiss:375218160225222656>")
+				.addField("!emote", "If you want to use nice emotes because being restricted to just 50 is no fun <:blobtear:375218561628766210>. Just pick an emote from the extensive [emote list](https://docs.google.com/spreadsheets/d/1VtGPAa2QJU5IUZdEdJpj4eQVoz4aMSS9yNqCCTsWpEI/edit?usp=sharing), similarily to the previous command this one is case sen-si-ti-ve~ <:blobkiss:375218160225222656>\n\n\n\nI hope this has been helpful to you and that you're gonna have a lot of fun playing with me from now on<:blobpeek:375218261660401664>\n\nOh, I almost forgot that I shouldn't speak about the secret commands & interactions, what a klutz I am teehee~");
+				message.channel.send({ embed: helpembed });
+		}	
+		
+	}
+	//getting the hello command to work with stuff at the end.
 	if(message.content.startsWith(auth.prefix + 'hello'))
 	{
 		special = message.author;
@@ -185,20 +289,20 @@ bot.on('message', (message) =>
 	}
 	
 	//the say command to make her say whatever you want.
-	if(message.content.startsWith(auth.prefix + 'say') && (!message.author.bot) )
+	if(message.content.startsWith(auth.prefix + 'say') && (!message.author.bot))
 	{
 		var txt = message.content.substring(5,message.length);
 		message.delete();
 		message.channel.send(txt + " " + emote);
 	}
-
+	
 	if(message.content.startsWith(auth.prefix + 'lit') && (!message.author.bot) )
 	{
 		var txt = message.content.substring(5,message.length);
 		message.channel.send(":ok_hand::fire::joy_cat::100:");
 	}
-
-		//the role command to get any role if available.	
+	
+	//the role command to get any role if available.	
 	if((message.content.startsWith(auth.prefix + 'role')) && (!message.author.bot))
 	{
 		console.log(auth.prefix + 'role command issued');
@@ -265,6 +369,7 @@ bot.on('message', (message) =>
 			break;
 		}
 	}
+	
 	//Getting access to every emote
 	if((message.content.startsWith(auth.prefix + 'emote')) && (!message.author.bot))
 	{
@@ -306,26 +411,8 @@ bot.on('message', (message) =>
 				//End of promise
 			}
 		});
-		urlExists(auth.emotes+cont+".jpg", function(err, exists)
-		{
-			if(exists)
-			{
-				bool = true;
-				//Promise
-				var promise = new Promise(function(resolve, reject) 
-				{
-					// do a thing, possibly async, then…
-					resolve(message.channel.send("", {file: auth.emotes + cont + ".jpg"}).catch(
-					function() 
-					{ 
-						console.log("promesse rompue");
-					}));
-				});
-				//End of promise
-			}
-		});
 	}
-		
+	
 	//reacting to "for you Carmilla messages"
 	if((message.content.startsWith('For you Carmilla')) && (!message.author.bot))
 	{
@@ -358,15 +445,6 @@ bot.on('message', (message) =>
 		message.channel.send('*blushes*' + special);
 	}
 	}
-	else
-	
-		
-		
-			
-		
-		
-	
-		
 	//reacting to posts in art-channel
 	if(((message.channel.id == auth.artchannel) && (message.attachments.size == 1)) && (!message.author.bot))
 	{
@@ -388,7 +466,6 @@ bot.on('message', (message) =>
 			message.react("375604900119314432").catch(void(0));
 		}
 	}
-		
 	//reacting to posts in salt-channel
 	if(((message.channel.id == auth.saltchannel) && (message.attachments.size == 1)) && (!message.author.bot))
 	{
@@ -398,7 +475,4 @@ bot.on('message', (message) =>
 			message.react("427404778436427786").catch(void(0));
         	}
 	}
-	//end of the if
-	}
-//end of the function
 });
